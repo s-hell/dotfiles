ca0d308f6d8c2fb5459b7e725ce03b5a1c49e981
shell <shell@pcspinnt.de>
2024-11-17 15:04:51 +0100

    Better agent starting

--- configs/.bash.d/31-ssh.bash
+++ configs/.bash.d/31-ssh.bash
@@ -1,8 +1,10 @@
 # Source SSH settings, if applicable
-if [ -f "${SSH_ENV}" ] && ( [ -f "~/.ssh/id_ecdsa" ] || [ -f "~/.ssh/id_rsa" ] ) ; then
+if [ -f "${SSH_ENV}" ]; then
     . "${SSH_ENV}" > /dev/null
     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-    start_agent;
+        if [ -f "~/.ssh/id_ecdsa" ] || [ -f "~/.ssh/id_rsa" ]; then
+            start_agent;
+        fi
 }
 else
 # don't start ssh-agent for root
